5edd4ce6eb94ef3260a0a689acda9bca5e15ad32
Alex <alexwaller301@gmail.com>
2022-02-09 21:10:37 -0500

    update cookies

--- my-tickets-frontend/pages/api/auth/login.js
+++ my-tickets-frontend/pages/api/auth/login.js
@@ -1,27 +1,34 @@
 // import {sign} from "jsonwebtoken"
 // var Cookies = require('cookies')
-import { parseCookies, setCookie, destroyCookie } from 'nookies'
-var cookieParser = require('cookie-parser')
-
+import { parseCookies, setCookie, destroyCookie } from "nookies";
+var cookieParser = require("cookie-parser");
+import { serialize } from "cookie";
+import Cookies from "js-cookie";
 
 export default async function handler(req, res) {
-    const {username, password} = JSON.parse(req.body)
+  const { username, password } = JSON.parse(req.body);
 
-    console.log(username)
-      const loginInfo = {
-      identifier: username,
-      password: password
-  }
+  const loginInfo = {
+    identifier: username,
+    password: password,
+  };
 
-  const login = await fetch('http://localhost:1337/api/auth/local', {
+  const login = await fetch("http://localhost:1337/api/auth/local", {
     method: "POST",
     headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
+      Accept: "application/json",
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(loginInfo)
-  })
+    body: JSON.stringify(loginInfo),
+  });
 
-  const loginResponse = await login.json()
-    res.status(200).json(loginResponse)
+  const loginResponse = await login.json();
+
+  setCookie({ res }, "jwt", loginResponse.jwt, {
+    secure: process.env.NODE_ENV === "production",
+    maxAge: 72576000,
+    httpOnly: true,
+    path: "/",
+  });
+  res.status(200).json({message: 'success'});
 }
