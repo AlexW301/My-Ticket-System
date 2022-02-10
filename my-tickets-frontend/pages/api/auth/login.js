a32d5ebabb34bcf6b960039862a800cedb0d6030
Alex <alexwaller301@gmail.com>
2022-02-10 06:51:10 -0500

    Update login.js

--- my-tickets-frontend/pages/api/auth/login.js
+++ my-tickets-frontend/pages/api/auth/login.js
@@ -1,18 +1,20 @@
 // import {sign} from "jsonwebtoken"
 // var Cookies = require('cookies')
 import { parseCookies, setCookie, destroyCookie } from "nookies";
-var cookieParser = require("cookie-parser");
-import { serialize } from "cookie";
-import Cookies from "js-cookie";
 
 export default async function handler(req, res) {
+  //Get current cookies
+  const cookies = parseCookies({ req });
+  console.log(cookies.jwt);
   const { username, password } = JSON.parse(req.body);
 
+  // Set log in info to the username and password provided
   const loginInfo = {
     identifier: username,
     password: password,
   };
 
+  //The "real" api call to strapi for authentication
   const login = await fetch("http://localhost:1337/api/auth/local", {
     method: "POST",
     headers: {
@@ -24,11 +26,14 @@ export default async function handler(req, res) {
 
   const loginResponse = await login.json();
 
+  // Sets the browsers http only cookie to the jwt recieved from strapi
   setCookie({ res }, "jwt", loginResponse.jwt, {
     secure: process.env.NODE_ENV === "production",
     maxAge: 72576000,
     httpOnly: true,
     path: "/",
   });
-  res.status(200).json({message: 'success'});
+
+  // Response to client
+  res.status(200).json({ message: "success" });
 }
