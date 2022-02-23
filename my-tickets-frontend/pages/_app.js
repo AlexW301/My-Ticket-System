4d6eed29e2a80c0ba5025d44da24beea7f5b251a
Alex <alexwaller301@gmail.com>
2022-02-22 20:01:22 -0500

    added logo

--- my-tickets-frontend/pages/_app.js
+++ my-tickets-frontend/pages/_app.js
@@ -1,9 +1,12 @@
 import "../styles/globals.scss";
-import Divider from '@mui/material/Divider';
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="icon" href="/images/pmllogo.png" />
+      </Head>
       <Component {...pageProps} />
     </>
   );
